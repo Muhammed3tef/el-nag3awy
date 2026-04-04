bcb13901ad8d2748cee3446630b562a9b74f3415
Muhammed Atef <atef21143@gmail.com>
2026-04-04 13:45:06 +0200

    add hero img section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import SubFooter from "@/components/footer/components/sub-footer";
 
+
+
+
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+       <div className="bg-[url('/imgs/hero-img.svg')]  bg-cover bg-center h-screen relative ">
+         <div className="absolute inset-0 bg-[#1D3F34] opacity-50"></div>
+        
+       </div>
       <SubFooter
         title="هل تريد بيع أثاثك اليوم؟"
         description="تواصل معنا الآن واحصل على أفضل سعر لأثاثك المستعمل مع خدمة نقل مجانية ودفع فوري"
