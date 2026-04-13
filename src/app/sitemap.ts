3884f6efefd7a334e12122cc2529bc27ce007531
Mariam Zeid <mariamzeidd@gmail.com>
2026-04-13 15:06:58 +0200

    add routes to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,16 +9,34 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${process.env.BASE_URL}/about`,
+      url: `${process.env.BASE_URL}/contact-us`,
       lastModified: new Date(),
-      changeFrequency: `monthly`,
+      changeFrequency: `yearly`,
+      priority: 0.7,
+    },
+    {
+      url: `${process.env.BASE_URL}/our-locations`,
+      lastModified: new Date(),
+      changeFrequency: `yearly`,
       priority: 0.8,
     },
     {
-      url: `${process.env.BASE_URL}/blog`,
+      url: `${process.env.BASE_URL}/furns`,
       lastModified: new Date(),
-      changeFrequency: `weekly`,
-      priority: 0.5,
+      changeFrequency: `yearly`,
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.BASE_URL}/kitchens`,
+      lastModified: new Date(),
+      changeFrequency: `yearly`,
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.BASE_URL}/who-are-we`,
+      lastModified: new Date(),
+      changeFrequency: `yearly`,
+      priority: 0.6,
     },
   ];
 }
